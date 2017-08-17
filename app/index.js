c7882553a4365544152e5d5ae815c4548e526a47
hexonZhang <308826842@qq.com>
2017-08-17 12:14:18 +0800

    一个基于webpack的react项目框架

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './Hello.js';
+import PropState from './PropState';
+
+ReactDOM.render(
+      <PropState />,
+  document.getElementById('root')
+);
